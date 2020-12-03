7f5c8d416eabd6e8e6446b365351ab21ba38b5b3
Leonid Shatilo <shatiloleonid@gmail.com>
2020-12-03 18:52:27 +0300

    feat: implement new functionality

--- src/script.js
+++ src/script.js
@@ -1,17 +1,39 @@
+/* Get Our Elements */
 const video = document.querySelector('.video'),
       speedBar = document.querySelector('.speed-bar'),
-      speedController = speedBar.querySelector('.speed-controller');
+      speedController = document.querySelector('.speed-controller'),
+      minSpeed = 0.5,
+      maxSpeed = 2.5;
 
-function handleMove(e) {
-  const y = e.pageY - this.offsetTop,
-        percent = y / this.offsetHeight,
-        min = 0.5,
-        max = 2.5,
-        height = Math.round(percent * 100) + '%',
-        playbackRate = percent * (max - min) + min;
-  speedController.style.height = height;
-  speedController.textContent = playbackRate.toFixed(2) + '×';
+/* Build out functions */
+function chooseSpeed(e) {
+  const Y = e.pageY - this.offsetTop,
+        percent = Y / this.offsetHeight;
+  if (percent > 1) {
+    percent = 1;
+  }
+  const itemHeight = `${Math.round(percent * 100)}%`,
+        playbackRate = percent * (maxSpeed - minSpeed) + minSpeed;
+  speedController.style.height = itemHeight;
+  speedController.innerHTML = `${playbackRate.toFixed(1)}x`;
+}
+
+function setSpeed(e) {
+  const Y = e.pageY - this.offsetTop,
+        percent = Y / this.offsetHeight,
+        playbackRate = percent * (maxSpeed - minSpeed) + minSpeed;
   video.playbackRate = playbackRate;
 }
 
-speedBar.addEventListener('mousemove', handleMove);
+function getCurrentSpeed(e) {
+  const currentSpeed = video.playbackRate,
+        percent = (currentSpeed - minSpeed) / (maxSpeed - minSpeed),
+        itemHeight = `${Math.round(percent * 100)}%`;
+  speedController.style.height = itemHeight;
+  speedController.innerHTML = `${currentSpeed.toFixed(1)}x`;
+}
+
+/* Hook up the event listeners */
+speedBar.addEventListener('mousemove', chooseSpeed);
+speedBar.addEventListener('mousedown', setSpeed);
+speedBar.addEventListener('mouseout', getCurrentSpeed);
